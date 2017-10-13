e17ac3e865c67ee1e356e5bd1b50998f93bd4cfa
Jesus Martin Castro <jesmartin82@gmail.com>
2017-10-13 08:59:58 +0200

    corrigiendo ejercicios

--- Tema_3/Ejercicio12.java
+++ Tema_3/Ejercicio12.java
@@ -11,14 +11,14 @@ public class Ejercicio12 {
       
       System.out.println("¿Quiere saber que nota debe sacar en su segundo examen para saber la nota que desea sacar en el trimestre?");
       System.out.println("Dígame la nota del primer examen y la nota que desea sacar al final de curso.");
-      System.out.println("¿Qué nota sacó en el primer examen?");
-        double notaPrimerExamen = Double.parseDouble(System.console().readLine());
-      System.out.println("Ahora ponga a nota que desea sacar en el curso");
-        double notaFinCurso = Double.parseDouble(System.console().readLine());
-        double importanciaPrimerExamen = (notaPrimerExamen * 40) / 100;
-        double restanteNota =  notaFinCurso - importanciaPrimerExamen;
-        double notaSegundoExamen = (restanteNota * 100) / 60;
+      System.out.println("¿Qué nota sacó en el primer examen?");//pedimos la nota que sacó en el primer examen
+        double notaPrimerExamen = Double.parseDouble(System.console().readLine());//variable para la nota del primer examen
+      System.out.println("Ahora ponga la nota que desea sacar en el trimestre");//solicitamos la nota que se desea sacar en el trimestre
+        double notaFinTrimestre = Double.parseDouble(System.console().readLine());//variable para la nota del trimestre
+        double importanciaPrimerExamen = (notaPrimerExamen * 40) / 100;//aquí calculamos el 40% de importancia del primer examen
+        double restanteNota =  notaFinTrimestre - importanciaPrimerExamen;//restamos la nota fin de trimestre con la nota del primer examen para saber la nota restante del 60% del segundo examen
+        double notaSegundoExamen = (restanteNota * 100) / 60;//dado el resultado de la resta anterior calculamos el total del segundo examen
         
-      System.out.println("La nota que debe sacar en el segundo examen deberá ser: " + notaSegundoExamen);
+      System.out.printf("La nota que debe sacar en el segundo examen deberá ser: %.2f", notaSegundoExamen);//mostramos el total de la nota que se ha de sacar en el segundo examen
     }
 }
--- Tema_3/Ejercicio2.java
+++ Tema_3/Ejercicio2.java
@@ -11,7 +11,7 @@ public class Ejercicio2 {
       System.out.println("Diga los Euros que necesita saber lo que son en pesetas: ");
         double euro = Double.parseDouble(System.console().readLine()); //aquí creamos la variable para la introducción del primer número que el usuario a introducido por teclado
         
-        double total = (int) (euro * 166.386); //con esta variable hacemos el cálculo para pasar de Euros a Pesetas la cantidad deseada.
+        int total = (int) (euro * 166.386); //con esta variable hacemos el cálculo para pasar de Euros a Pesetas la cantidad deseada.
         
         System.out.println(euro + " Euros son:  " + total + " pesetas"); //mostramos el resultado de la conversion
         
