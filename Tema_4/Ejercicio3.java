b61c4aac50b862f3542866b662e442b87c02a6d6
Jesus Martin Castro <jesmartin82@gmail.com>
2017-10-18 11:24:40 +0200

    corrigiendo el ejercicio 3

--- Tema_4/Ejercicio3.java
+++ Tema_4/Ejercicio3.java
@@ -6,18 +6,34 @@
 */
 public class Ejercicio3 {
     public static void main(String[] args) {
-      System.out.print("¿Qué hora es? ");
-        int hora = Integer.parseInt (System.console().readLine());//Pedimos la hora y la metemos en esta variable
+      System.out.print("¿Qué día es? (métalo de manera numérica) ");
+        int dia = Integer.parseInt (System.console().readLine());
         
-        if ((hora >=6) && (hora <=12)) { // Con esta sentencia le decimos que si la hora introducida está entre ese rango, nos de los buenos días.
-            System.out.println("Buenos días");
-        } else if ((hora >=13) && (hora<= 20)){ // Con esta otra sentencia la dedicamos a dar las buenas tardes
-            System.out.println("Buenas tardes");
-        }else if ((hora >=21) && (hora<= 24)){ // Para las buenas noches hemos creado dos posibilidades, dado que el rango de horas que se da no es consecutivo entre si, de ahí que tengamos esta primera que va desde las 21 hasta las 24
-            System.out.println("Buenas noches");
-        }else if ((hora >=00) && (hora<= 5)){
-            System.out.println("Buenas noches"); // y esta otra que va desde las 00 hasta las 05
-        }else {
-        }
+        switch (dia) {
+          
+          case 1:
+            System.out.println("Hoy es Lunes");
+            break;
+          case 2:
+            System.out.println("Hoy es Martes");
+            break;
+          case 3:
+            System.out.println("Hoy es Miércoles");
+            break;
+          case 4:
+            System.out.println("Hoy es Jueves");
+            break;
+          case 5:
+            System.out.println("Hoy es Viernes");
+            break;
+          case 6:
+            System.out.println("Hoy es Sábado");
+            break;
+          case 7:
+            System.out.println("Hoy es Domingo");
+            break;
+        default:
+            System.out.println("Esa opción no exite");
+      }
     }
 }
