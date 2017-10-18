c1a209065dcc5f4a4f825f3fb2eba9f225ee64ca
Jesus Martin Castro <jesmartin82@gmail.com>
2017-10-19 00:23:26 +0200

    Ejercicio 5 acabado

--- Tema_4/Ejercicio5.java
+++ Tema_4/Ejercicio5.java
@@ -1,25 +1,30 @@
 /**
-* Vamos a ampliar uno de los ejercicios de la relación anterior para considerar
-* las horas extras. Escribe un programa que calcule el salario semanal de un
-* trabajador teniendo en cuenta que las horas ordinarias (40 primeras horas de
-* trabajo) se pagan a 12 euros la hora. A partir de la hora 41, se pagan a 16
-* euros la hora.
+* Realiza un programa que resuelva una ecuación de primer grado (del tipo ax +
+* b = 0).
 *
 * @author Jesus Martin Castro
 */
 public class Ejercicio5 {
     public static void main(String[] args) {
-      System.out.print("Dígame el número de horas trabajadas y le diré su salario ");
-      System.out.println("a razón de 12 € las 40 primeras horas y a 16 € las restantes horas.");
-      System.out.print("¿Cuántas horas echó? ");
-        int horas = Integer.parseInt(System.console().readLine());
-      if (horas <= 40) {  
-      System.out.println("El total a cobrar por " + horas + " es de: " + horas*12 + " Euros");
-      } else if (horas > 40) {
-        int horaMayor = horas - 40;
-        int totalHoras = (40 * 12) + (horaMayor * 16);
-      System.out.println("El total a cobrar por " + horas + " es de: " + totalHoras + " Euros");
-      }else{
+      System.out.println("Vamos a resolver la siguien ecuación ax + b = 0 ");
+        String linea = System.console().readLine();
+      System.out.print("Diga el primer número ");
+        int primerNumero = Integer.parseInt(System.console().readLine());
+      System.out.print("Diga el segundo número ");
+        int segundoNumero = Integer.parseInt(System.console().readLine());
+        
+        if ((primerNumero == 0) && (segundoNumero == 0)) {
+        System.out.println("Estos no son números para resolver la ecuación");
+      } else {
+      }
+        double resultado = - primerNumero / segundoNumero;
+      if (resultado == 0) {  
+      System.out.println("El resultado de su operación es 0, ¡¡FELICIDADES!!");
+      } else if (resultado > 0) {
+        System.out.println("El resultado de su operación es " + resultado + " Lo siento pero no es un buen resultado.");
+      } else if (resultado < 0) {
+        System.out.println("El resultado de su operación es " + resultado + " Lo siento pero no es un buen resultado.");
+      } else {
     }
   }
 }
