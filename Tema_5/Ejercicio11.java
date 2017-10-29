44814c9bef04ff257808dcab1d4e60a369587ffb
Jesus Martin Castro <jesmartin82@gmail.com>
2017-10-29 13:23:01 +0100

    Ejercicio 11 Tema 5

--- Tema_5/Ejercicio11.java
+++ Tema_5/Ejercicio11.java
@@ -1,10 +1,35 @@
 /**
-* Escribe un programa que calcule la media de un conjunto de números positivos
-* introducidos por teclado. A priori, el programa no sabe cuántos números se
-* introducirán. El usuario indicará que ha terminado de introducir los datos
-* cuando meta un número negativo.
+* Escribe un programa que muestre en tres columnas, el cuadrado y el cubo de
+* los 5 primeros números enteros a partir de uno que se introduce por teclado.
 *
 * @author Jesus Martin Castro
 */
 public class Ejercicio11{
   public static void main(String[] args) {
+    
+    System.out.print("Escriba un número y le calcularé el cuadrado y el cubo de ");
+    System.out.print(" ese número y sus 5 consiguientes. ");
+      int numero = Integer.parseInt(System.console().readLine());
+    
+      int numeroCuadrado= 0;
+      int numeroCubo= 0;
+      int control = 0;
+    
+    System.out.printf("%-10s %-10s %-8s", "Numero", "Cubo²", "Cuadrado³\n");
+    System.out.println("-------------------------------");
+    
+      do {
+        
+        numeroCuadrado = numero * numero;
+        numeroCubo = numero * numero * numero;
+        
+        System.out.printf("%3d %10d %13d\n", numero, numeroCuadrado, numeroCubo);
+        System.out.println("-------------------------------");
+        
+        numero ++;
+        
+        control ++;
+        
+      } while (control <= 4);    
+  }
+}
