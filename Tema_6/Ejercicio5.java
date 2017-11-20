4b30b9e49f6c9cf29b32223834aec1c4347ba424
Jesus Martin Castro <jesmartin82@gmail.com>
2017-11-20 19:22:02 +0100

    Más ejercicios Tema 6

--- Tema_6/Ejercicio5.java
+++ Tema_6/Ejercicio5.java
@@ -9,10 +9,32 @@ public class Ejercicio5 {
   public static void main(String[] args) {
     System.out.print("Le mostraré 50 números de manera aleatoria entre el 100 y el 199:");
     String salto = System.console().readLine();
-        
-    for (int j = 1; j <= 50; j++) {
-    int numerosAleatorios = (int)(Math.random()*100) + 100;
-    System.out.print(" " + numerosAleatorios);
-    }
+    
+    int numMax = Integer.MIN_VALUE;
+    int numMin = Integer.MAX_VALUE;
+    double suma = 0;
+    int numerosAleatorios = 0;
+    int contador = 0;
+    
+          for (int j = 1; j <= 50; j++) {
+            numerosAleatorios = (int)(Math.random()*100) + 100;
+            System.out.print(" " + numerosAleatorios);
+              suma = suma + numerosAleatorios;
+                if (numerosAleatorios > numMax) { 
+                    numMax = numerosAleatorios;
+                } 
+                if (numerosAleatorios < numMin) {
+                    numMin = numerosAleatorios;
+                }
+              contador ++;
+          }
+          
+          double media = suma / contador;
+          System.out.println();
+          System.out.println("Maximo " + numMax);
+          System.out.println("Minimo " + numMin);
+          System.out.println("Suma " + suma);
+          System.out.println("Cantidad números introducidos " + contador);
+          System.out.println("Media " + media);
   }
 }
