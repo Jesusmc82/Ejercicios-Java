c52e5a0dac6f430df91b027418df1874c510bef0
Jesus Martin Castro <jesmartin82@gmail.com>
2017-11-21 20:54:10 +0100

    Subiendo ejercicio 11

---
+++ Tema_6/Ejercicio11.java
@@ -0,0 +1,46 @@
+/**
+* Escribe un programa que muestre 20 notas generadas al azar. Las notas deben
+* aparecer de la forma: suspenso, suficiente, bien, notable o sobresaliente. Al
+* final aparecerá el número de suspensos, el número de suficientes, el número
+* de bienes, etc.
+*
+* @author Jesus Martin Castro
+*/
+public class Ejercicio11 {
+  public static void main(String[] args) {
+  
+    int nota = 0;
+    int numeroNota = 20;
+    int suspenso = 0;
+    int suficiente = 0;
+    int bien= 0;
+    int notable= 0;
+    int sobresaliente= 0;
+    
+    for (int i = 1; i<= numeroNota; i++) {
+      nota = (int)(Math.random()*11);
+      if ((nota >= 0) && (nota <= 4)) {
+        System.out.println("Suspensos");
+        suspenso++;
+      } else if (nota == 5) {
+        System.out.println("Suficiente");
+        suficiente++;
+      } else if (nota == 6) {
+        System.out.println("Bien");
+        bien++;
+      } else if ((nota >= 7) && (nota <= 8)) {
+        System.out.println("Notable");
+        notable++;
+      } else if ((nota >= 9) && (nota <= 10)) {
+        System.out.println("Sobresaliente");
+        sobresaliente++;
+      }
+    } 
+      System.out.println("___________________");
+      System.out.println("Total suspensos= " + suspenso);
+      System.out.println("Total suficiente= " + suficiente);
+      System.out.println("Total Bien= " + bien);
+      System.out.println("Total Notable= " + notable);
+      System.out.println("Total Sobresaliente= " + sobresaliente);
+  }
+}
