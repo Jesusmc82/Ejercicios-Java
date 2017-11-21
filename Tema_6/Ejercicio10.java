611b0a612f4efd194f3cccea7ebb11985e3a5b0c
Jesus Martin Castro <jesmartin82@gmail.com>
2017-11-21 19:25:02 +0100

    Creando Ejercicio 10

---
+++ Tema_6/Ejercicio10.java
@@ -0,0 +1,13 @@
+/**
+* Realiza un programa que vaya generando números aleatorios pares entre 0
+* y 100 y que no termine de generar números hasta que no saque el 24. El
+* programa deberá decir al final cuántos números se han generado. 
+*
+* @author Jesus Martin Castro
+*/
+public class Ejercicio10 {
+  public static void main(String[] args) {
+  
+  
+  }
+}
