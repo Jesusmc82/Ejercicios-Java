67349bb254590c235892335b80465fde86c29d5d
Jesus Martin Castro <jesmartin82@gmail.com>
2017-11-21 19:56:23 +0100

    Subiendo ejercicio 10

--- Tema_6/Ejercicio10.java
+++ Tema_6/Ejercicio10.java
@@ -8,6 +8,36 @@
 public class Ejercicio10 {
   public static void main(String[] args) {
   
-  
+    int linea = 0;
+    int fila = 10;
+    
+    for (int i = 1; i<= fila; i++) {
+      linea = (int)(Math.random()*41)+1;
+      for (int j = 1; j <= linea; j++) {
+        int dibujo = (int)(Math.random()*7);
+        switch(dibujo) {
+          case 1:
+            System.out.print("*");
+          break;
+          case 2:
+            System.out.print("-");
+          break;
+          case 3:
+            System.out.print("=");
+          break;
+          case 4:
+            System.out.print(".");
+          break;
+          case 5:
+            System.out.print("|");
+          break;
+          case 6:
+            System.out.print("@");
+          break;
+        default:
+        }
+      }
+     System.out.println();
+    }
   }
 }
