f0beed0b6734965c8975302eb6cd35aacc1fd4d0
Jesus Martin Castro <jesmartin82@gmail.com>
2018-04-19 09:33:03 +0200

    Subiendo nuevos ejercicios

---
+++ Tema_10/EjercicioExamen/src/ejercicioexamen/EjercicioExamen.java
@@ -0,0 +1,61 @@
+package ejercicioexamen;
+
+/**
+ * Prueba Ejercicio del Examen de Programación 2015 2016 t3c1_b ejercicio 4
+ *
+ * @author jesus
+ */
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class EjercicioExamen {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        HashMap<String, String> plantaCientifica = new HashMap<>();
+        HashMap<String, String> plantaPropiedades = new HashMap<>();
+        HashMap<String, String> plantaMinuscula = new HashMap<>();
+        String plantaReferencia;
+
+        plantaCientifica.put("Ortiga", "Urtica dioica");
+        plantaCientifica.put("Romero", "Rosmarinus officinalis");
+        plantaCientifica.put("Aloe Vera", "Aloe barbadensis miller");
+        plantaCientifica.put("Ayahuasca", "Banisteriopsis caapi");
+
+        plantaPropiedades.put("Ortiga", "Propiedades 1");
+        plantaPropiedades.put("Romero", "Propiedades 2");
+        plantaPropiedades.put("Aloe Vera", "Propiedades 3");
+        plantaPropiedades.put("Ayahuasca", "Propiedades 4");
+
+        for (Map.Entry pareja : plantaPropiedades.entrySet()) {
+            plantaMinuscula.put((pareja.getKey().toString().toLowerCase()), pareja.getKey().toString());
+        }
+        
+        int contador = 1;
+
+        for (Map.Entry pareja : plantaCientifica.entrySet()) {
+            
+            if (contador < plantaCientifica.size()) {
+                System.out.print(pareja.getKey() + ", ");
+            } else {
+                System.out.print(pareja.getKey());
+            }
+            
+            contador++;
+        }
+
+        System.out.println();
+        System.out.print("Por favor, introduzca el nombre de una planta: ");
+        plantaReferencia = s.nextLine().toLowerCase();
+        System.out.println();
+
+        if (plantaMinuscula.containsKey(plantaReferencia)) {
+            System.out.println("Nombre común: " + plantaMinuscula.get(plantaReferencia));
+            System.out.println("Nombre científico: " + plantaCientifica.get(plantaMinuscula.get(plantaReferencia)));
+            System.out.println("Propiedades: " + plantaPropiedades.get(plantaMinuscula.get(plantaReferencia)));
+        } else {
+            System.out.println("Lo siento, esa planta no existe");
+        }
+    }
+}
---
+++ Tema_10/EjercicioExamen/test/ejercicioexamen/plantaPropiedadesTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioexamen;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jesus
+ */
+public class plantaPropiedadesTest {
+    
+    public plantaPropiedadesTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of put method, of class plantaPropiedades.
+     */
+    @Test
+    public void testPut() {
+        System.out.println("put");
+        String ortiga = "";
+        String urtica_dioica = "";
+        plantaPropiedades.put(ortiga, urtica_dioica);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
