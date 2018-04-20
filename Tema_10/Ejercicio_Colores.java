8b86ba203719e9145f777eace97be0080e22ddb7
Jesus Martin Castro <jesmartin82@gmail.com>
2018-04-20 09:17:27 +0200

    corrigiendo errores

--- Tema_10/Ejercicio_Colores.java
+++ Tema_10/Ejercicio_Colores.java
@@ -1,4 +1,4 @@
-package colores;
+//package colores;
 
 /**
  * Prueba Ejercicio del Examen de Programación 2015 2016 t3c1_a ejercicio 1
@@ -9,15 +9,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Colores {
+public class Ejercicio_Colores {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        HashMap<String, String> colorEspanol = new HashMap<>();
-        HashMap<String, String> colorRgb = new HashMap<>();
-        HashMap<String, String> colorCompleto = new HashMap<>();
-        String colores;
 
+		HashMap<String, String> colorEspanol = new HashMap<>();
         colorEspanol.put("Azul", "Blue");
         colorEspanol.put("Negro", "Black");
         colorEspanol.put("Rojo", "Red");
@@ -27,7 +24,8 @@ public class Colores {
         colorEspanol.put("Amarillo", "Yellow");
         colorEspanol.put("Naranja", "Orange");
         colorEspanol.put("Rosa", "Pink");
-
+		
+		HashMap<String, String> colorRgb = new HashMap<>();
         colorRgb.put("Azul", "#0000FF");
         colorRgb.put("Negro", "#000000");
         colorRgb.put("Rojo", "#FF0000");
@@ -37,27 +35,33 @@ public class Colores {
         colorRgb.put("Amarillo", "#FFFF00");
         colorRgb.put("Naranja", "#FFA500");
         colorRgb.put("Rosa", "#EE82EE");
-
+        
+        //En el diccionario colorCompleto almacenamos
+        //el nombre común en minúscula junto con el nombre
+        //original, por ejemplo ("rojo", "Rojo")
+        HashMap<String, String> colorCompleto = new HashMap<>();
         for (Map.Entry pareja : colorEspanol.entrySet()) {
             colorCompleto.put((pareja.getKey().toString().toLowerCase()), pareja.getKey().toString());
         }
 
-        int contador = 1;
+		//Muestra todos los nombres comunes para que el
+		//usuario sepa cuáles están disponibles.
+        int contadorColores = 1;
 
         for (Map.Entry pareja : colorEspanol.entrySet()) {
 
-            if (contador < colorEspanol.size()) {
+            if (contadorColores < colorEspanol.size()) {
                 System.out.print(pareja.getKey() + ", ");
             } else {
-                System.out.print(pareja.getKey());
+                System.out.print(pareja.getKey() + ("."));
             }
 
-            contador++;
+            contadorColores++;
         }
 
         System.out.println();
         System.out.print("Por favor, introduzca un color: ");
-        colores = s.nextLine().toLowerCase();
+        String colores = s.nextLine().toLowerCase();
         
         if (colorCompleto.containsKey(colores)) {
             System.out.println("El color " + colorCompleto.get(colores) + ", en inglés es " + colorEspanol.get(colorCompleto.get(colores)) + ", " + "y su codigo RGB es " +  colorRgb.get(colorCompleto.get(colores)));
--- Tema_10/Ejercicio_Planta.java
+++ Tema_10/Ejercicio_Planta.java
@@ -1,4 +1,4 @@
-package planta;
+//package planta;
 
 /**
  * Prueba Ejercicio del Examen de Programación 2015 2016 t3c1_b ejercicio 4
@@ -9,45 +9,49 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class planta {
+public class Ejercicio_Planta {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        HashMap<String, String> plantaCientifica = new HashMap<>();
-        HashMap<String, String> plantaPropiedades = new HashMap<>();
-        HashMap<String, String> plantaMinuscula = new HashMap<>();
-        String plantaReferencia;
-
+        
+		HashMap<String, String> plantaCientifica = new HashMap<>();
         plantaCientifica.put("Ortiga", "Urtica dioica");
         plantaCientifica.put("Romero", "Rosmarinus officinalis");
         plantaCientifica.put("Aloe Vera", "Aloe barbadensis miller");
         plantaCientifica.put("Ayahuasca", "Banisteriopsis caapi");
+		
+		HashMap<String, String> plantaPropiedades = new HashMap<>();
+        plantaPropiedades.put("Ortiga", "Es diurética y depurativa. Baja los niveles de colesterol.");
+        plantaPropiedades.put("Romero", "Tiene efectos estimulantes y tónicos, favorece en la recuperación de las enfermedades respiratorias y del aparato digestivo.");
+        plantaPropiedades.put("Aloe Vera", "Regenera las células de la piel e incluso las de tejidos internos. Elimina hongos y virus. Protege el sistema inmunitario.");
+        plantaPropiedades.put("Ayahuasca", "Se usa en el tratamiento de la depresión y la ansiedad así como en los problemas de personalidad y esquizofrenia.");
 
-        plantaPropiedades.put("Ortiga", "Propiedades 1");
-        plantaPropiedades.put("Romero", "Propiedades 2");
-        plantaPropiedades.put("Aloe Vera", "Propiedades 3");
-        plantaPropiedades.put("Ayahuasca", "Propiedades 4");
-
+        
+        //En el diccionario plantaMinuscula almacenamos
+        //el nombre común en minúscula junto con el nombre
+        //original, por ejemplo ("ortiga", "Ortiga")
+        HashMap<String, String> plantaMinuscula = new HashMap<>();
         for (Map.Entry pareja : plantaPropiedades.entrySet()) {
             plantaMinuscula.put((pareja.getKey().toString().toLowerCase()), pareja.getKey().toString());
         }
 
-        int contador = 1;
-
+		//Muestra todos los nombres comunes para que el
+		//usuario sepa cuáles están disponibles.
+        int contadorPlanta = 1;
         for (Map.Entry pareja : plantaCientifica.entrySet()) {
 
-            if (contador < plantaCientifica.size()) {
+            if (contadorPlanta < plantaCientifica.size()) {
                 System.out.print(pareja.getKey() + ", ");
             } else {
-                System.out.print(pareja.getKey());
+                System.out.print(pareja.getKey() + ("."));
             }
 
-            contador++;
+            contadorPlanta++;
         }
 
         System.out.println();
         System.out.print("Por favor, introduzca el nombre de una planta: ");
-        plantaReferencia = s.nextLine().toLowerCase();
+        String plantaReferencia = s.nextLine().toLowerCase();
 
         if (plantaMinuscula.containsKey(plantaReferencia)) {
             System.out.println("Nombre común: " + plantaMinuscula.get(plantaReferencia));
